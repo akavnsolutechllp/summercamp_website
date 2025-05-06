19a2654c205a9b1d5f6ac798ade10c2cbbfb1ff6
Prarthan_Akavn <akavnsolutechllp1@gmail.com>
2025-05-06 12:43:39 +0530

    updated v13

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 dotenv.config();
 
