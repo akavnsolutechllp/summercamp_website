1b0d28ed00540caa1d2085a88092d16f4cadf97d
Prarthan_Akavn <akavnsolutechllp1@gmail.com>
2025-05-06 16:13:23 +0530

    updated v21

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,4 +6,10 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),tailwindcss()],
   base: '/',
+  routes: [
+    {
+      "src": "/(.*)",
+      "dest": "/index.html"
+    }
+  ]
 })
