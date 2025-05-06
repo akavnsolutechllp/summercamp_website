14a3c7ca7d72a4904624259d3d44e556049b9647
Prarthan_Akavn <akavnsolutechllp1@gmail.com>
2025-05-06 14:32:46 +0530

    updated v16

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { FormDataProvider } from './context/FormDataContext.jsx';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <FormDataProvider>
+      <BrowserRouter>
     <App />
+      </BrowserRouter>
   </FormDataProvider>
   </StrictMode>
 )
