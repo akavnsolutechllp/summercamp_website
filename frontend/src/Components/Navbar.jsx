a8bbb2e6b92e6d64b3e50a3f2fe6c18884c637ed
Prarthan23 <145659525+Prarthan23@users.noreply.github.com>
2026-01-06 20:14:32 +0530

    development: changes in navbar

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -34,10 +34,10 @@ const Navbar = () => {
   };
 
   return (
-    <div className="w-full h-[12vh] lg:h-[30vh] flex justify-center items-center 2xl:justify-center px-4 relative z-20 overflow-hidden xl:overflow-hidden">
+    <div className="w-full h-[12vh] lg:h-[40vh] flex justify-center items-center 2xl:justify-center px-4 relative z-20 overflow-hidden xl:overflow-hidden">
       <img src={banner} alt="navbar_banner" fetchPriority="high" className="w-full absolute object-cover" />
 
-      <div className="hidden lg:w-[100%] xl:w-[100%] 2xl:w-[100%] md:flex lg:flex justify-center gap-24 items-center ml-auto text-white font-semibold text-lg z-10 mt-72 lg:mt-40  p-4">
+      <div className="hidden lg:w-[100%] xl:w-[100%] 2xl:w-[100%] md:flex lg:flex justify-center gap-24 items-center ml-auto text-white font-semibold text-lg z-10 mt-72 lg:mt-52  p-4">
         <button
           onClick={handleHome}
           className={`${location.pathname === "/" ? "bg-white text-[#FF0066] border-[#FF0066]" : "bg-[#FF0066] text-white border-white"} border-2 px-4 py-2 rounded-full xl:hover:scale-105 xl:hover:cursor-pointer ease-in-out`}
